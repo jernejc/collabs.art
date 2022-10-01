de189794c4110f96afc43546694d01c3544423a7
Jernej <jernej.co@gmail.com>
2022-10-01 22:14:13 +0200

    test: signature reuse should fail

--- test/contracts/CollabToken.js
+++ test/contracts/CollabToken.js
@@ -16,50 +16,50 @@ contract("CollabToken tests", async accounts => {
   });
 
   it("should credit 1 ETH", async () => {
-      try {
-        // credit account[1] with some $PXT
-        await tokenInstance.credit({ from: accounts[1], value: web3.utils.toWei('1', 'ether') });
-  
-        // Verify account balance
-        const finalBalanceAccount1 = await tokenInstance.balanceOf(accounts[1]);
-        expect(finalBalanceAccount1.toString()).to.equal(web3.utils.toWei('500', 'ether')); // $PXT tokens use the same decimals as ETH
-        // Verify contract balance
-        const finalBalanceContract = await web3.eth.getBalance(tokenInstance.address);
-        expect(finalBalanceContract.toString()).to.equal(web3.utils.toWei('1', 'ether'));
-      } catch (error) {
-        console.error(error);
-        assert.fail("One or more errors occured.");
-      }
-    });
-  
-    it("should withdraw 0.5 ETH", async () => {
-      try {
-        const initialBalanceContract = new BN(await web3.eth.getBalance(tokenInstance.address));
-        const initialBalanceAccount0 = new BN(await web3.eth.getBalance(accounts[0]));
-  
-        // credit account[1] with some $PXT
-        const response = await tokenInstance.withdraw(web3.utils.toWei('0.5', 'ether'));
-  
-        // get gas used
-        const gasUsed = new BN(response.receipt.gasUsed);
-  
-        // Get gas price
-        const tx = await web3.eth.getTransaction(response.tx);
-        const gasPrice = new BN(tx.gasPrice);
-        const adjustedInitialBalanceAccount0 = initialBalanceAccount0.sub(gasUsed.mul(gasPrice));
-  
-        // Verify account balance
-        const finalBalanceAccount0 = new BN(await web3.eth.getBalance(accounts[0]));
-        expect(finalBalanceAccount0.toString()).to.equal(adjustedInitialBalanceAccount0.add(new BN(web3.utils.toWei('0.5', 'ether'))).toString());
-  
-        // Verify contract balance
-        const finalBalanceContract = await web3.eth.getBalance(tokenInstance.address);
-        expect(finalBalanceContract.toString()).to.equal(initialBalanceContract.sub(new BN(web3.utils.toWei('0.5', 'ether'))).toString());
-      } catch (error) {
-        console.error(error);
-        assert.fail("One or more errors occured.");
-      }
-    });
+    try {
+      // credit account[1] with some $PXT
+      await tokenInstance.credit({ from: accounts[1], value: web3.utils.toWei('1', 'ether') });
+
+      // Verify account balance
+      const finalBalanceAccount1 = await tokenInstance.balanceOf(accounts[1]);
+      expect(finalBalanceAccount1.toString()).to.equal(web3.utils.toWei('500', 'ether')); // $PXT tokens use the same decimals as ETH
+      // Verify contract balance
+      const finalBalanceContract = await web3.eth.getBalance(tokenInstance.address);
+      expect(finalBalanceContract.toString()).to.equal(web3.utils.toWei('1', 'ether'));
+    } catch (error) {
+      console.error(error);
+      assert.fail("One or more errors occured.");
+    }
+  });
+
+  it("should withdraw 0.5 ETH", async () => {
+    try {
+      const initialBalanceContract = new BN(await web3.eth.getBalance(tokenInstance.address));
+      const initialBalanceAccount0 = new BN(await web3.eth.getBalance(accounts[0]));
+
+      // credit account[1] with some $PXT
+      const response = await tokenInstance.withdraw(web3.utils.toWei('0.5', 'ether'));
+
+      // get gas used
+      const gasUsed = new BN(response.receipt.gasUsed);
+
+      // Get gas price
+      const tx = await web3.eth.getTransaction(response.tx);
+      const gasPrice = new BN(tx.gasPrice);
+      const adjustedInitialBalanceAccount0 = initialBalanceAccount0.sub(gasUsed.mul(gasPrice));
+
+      // Verify account balance
+      const finalBalanceAccount0 = new BN(await web3.eth.getBalance(accounts[0]));
+      expect(finalBalanceAccount0.toString()).to.equal(adjustedInitialBalanceAccount0.add(new BN(web3.utils.toWei('0.5', 'ether'))).toString());
+
+      // Verify contract balance
+      const finalBalanceContract = await web3.eth.getBalance(tokenInstance.address);
+      expect(finalBalanceContract.toString()).to.equal(initialBalanceContract.sub(new BN(web3.utils.toWei('0.5', 'ether'))).toString());
+    } catch (error) {
+      console.error(error);
+      assert.fail("One or more errors occured.");
+    }
+  });
 
   it("should permit 100 transfer", async () => {
 
@@ -105,8 +105,17 @@ contract("CollabToken tests", async accounts => {
       expect(finalBalanceWalletProvider.toString()).to.equal(initialBalanceWalletProvider.sub(new BN(web3.utils.toWei('1', 'ether'))).toString());
       const finalBalanceAccount4 = await tokenInstance.balanceOf(accounts[4]);
       expect(finalBalanceAccount4.toString()).to.equal(initialBalanceAccount4.add(new BN(web3.utils.toWei('1', 'ether'))).toString());
+
+      // Make sure signature cannot be re-used
+      const detectedError = "CollabToken: invalid signature";
+      try {
+        await tokenInstance.grant(provider.addresses[0], accounts[4], web3.utils.toWei('1', 'ether'), result.deadline, 'twitter:login', result.v, result.r, result.s, {
+          from: accounts[4]
+        });
+      } catch (error) {
+        expect(error.toString()).to.contain(detectedError);
+      }
     } catch (error) {
-      console.error(error);
       assert.fail("One or more errors occured.");
     }
   });
