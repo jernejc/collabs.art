972034aeae68a49267193e09124b65892d31e788
Jernej <jernej.co@gmail.com>
2023-03-01 16:45:30 +0100

    fix: docs links

--- client/util/config.js
+++ client/util/config.js
@@ -113,12 +113,12 @@ const config = {
     defaultNotificationTime: 5000,
     btnColors: ['green', 'orange', 'blue', 'white'],
     btnIcons: ['ethereum-logo', 'link', 'info-text'],
-    docsLink: 'https://www.notion.so/Collabs-0c8e5af1deee4b6982b1c70fd354c9fc',
+    docsLink: 'https://collabsart.notion.site/Collabs-0c8e5af1deee4b6982b1c70fd354c9fc',
     docs: {
-      link: 'https://www.notion.so/Collabs-0c8e5af1deee4b6982b1c70fd354c9fc',
-      getColabLink: "https://www.notion.so/collabsart/Collabs-0c8e5af1deee4b6982b1c70fd354c9fc#502e4f2338344a3b8bdd6f903470dd3b",
-      auctionLifecycleLink: "https://www.notion.so/collabsart/Auction-Lifecycle-efd602f095e14de690af4f77600e1e04",
-      coloringPixelsLink: "https://www.notion.so/collabsart/Coloring-pixels-0fc42efade074114b823c67ff5874fd2"
+      link: 'https://collabsart.notion.site/Collabs-0c8e5af1deee4b6982b1c70fd354c9fc',
+      getColabLink: "https://collabsart.notion.so/Collabs-0c8e5af1deee4b6982b1c70fd354c9fc#502e4f2338344a3b8bdd6f903470dd3b",
+      auctionLifecycleLink: "https://collabsart.notion.so/Auction-Lifecycle-efd602f095e14de690af4f77600e1e04",
+      coloringPixelsLink: "https://collabsart.notion.so/Coloring-pixels-0fc42efade074114b823c67ff5874fd2"
     },
   },
   slideshow: {
